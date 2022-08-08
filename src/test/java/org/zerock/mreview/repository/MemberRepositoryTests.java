84868f72516e7af78802c77ba1d18593377e88dc
cimaron <cimaron713@gmail.com>
2022-08-08 10:16:43 +0900

    master

---
+++ src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -0,0 +1,32 @@
+package org.zerock.mreview.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.zerock.mreview.entity.Member;
+
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+    @Autowired
+    private MemberRepository memberRepository;
+
+//    @Test
+//    public void insertMembers() {
+//        IntStream.rangeClosed(1,100).forEach(i -> {
+//            Member member = Member.builder()
+//                    .email("r"+i +"@zerock.org")
+//                    .pw("1111")
+//                    .nickname("reviewer"+i).build();
+//            memberRepository.save(member);
+//        });
+//    }
+
+
+}
